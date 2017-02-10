509a8149fc9032eecd82c442b8ab44f209603ed5
Bekhzod Umarov <theimpaler@legend>
2017-02-10 11:27:32 -0500

    cleanup

--- nmap-gui.py
+++ nmap-gui.py
@@ -271,30 +271,3 @@ if __name__=='__main__':
     g = urGui(ip=ip)
     g.run()
 
-
-def factor():
-    factor_me = 9278317281738917298371937112213123131
-
-    output_widget = urwid.Text("Factors of %d:\n" % factor_me)
-    edit_widget = urwid.Edit("Type anything or press enter to exit:")
-    frame_widget = urwid.Frame(
-        header=edit_widget,
-        body=urwid.Filler(output_widget, valign='bottom'),
-        focus_part='header')
-
-    def exit_on_enter(key):
-        if key == 'enter': raise urwid.ExitMainLoop()
-
-    loop = urwid.MainLoop(frame_widget, unhandled_input=exit_on_enter)
-
-    def received_output(data):
-        output_widget.set_text( output_widget.text + data.decode('ascii') )
-
-    write_fd = loop.watch_pipe(received_output)
-    proc = subprocess.Popen(
-        ['ping', '172.29.27.184'],
-        stdout=write_fd,
-        close_fds=True)
-
-    proc.kill()
-
